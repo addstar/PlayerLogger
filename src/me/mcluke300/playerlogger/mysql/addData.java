619d818d3698a3a5e1558cd57e4ed5acdc7f590c
addstar <adam@addstar.com.au>
2014-03-13 14:40:46 +1100

    Use async tasks for DB updates (in future this will be changed to batch updates)

--- src/me/mcluke300/playerlogger/mysql/addData.java
+++ src/me/mcluke300/playerlogger/mysql/addData.java
@@ -4,39 +4,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import org.bukkit.Bukkit;
+
+import me.mcluke300.playerlogger.playerlogger;
 import me.mcluke300.playerlogger.config.*;
 
 public class addData {
+	playerlogger plugin;
+	Connection con = null;
+
+	public addData(playerlogger instance) {
+		plugin = instance;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://" + getConfig.MySQLServer() + "/" + getConfig.MySQLDatabase() , getConfig.MySQLUser(), getConfig.MySQLPassword());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	//MySQL
-	public static void add(String playername, String type, String data, double x, double y, double z, String worldname, Boolean staff) {
-		PreparedStatement pst = null;
-		Connection con = null;
-		long time = System.currentTimeMillis()/1000; //Unix time
-		//Checking if they should be logged
-		if (staff && getConfig.LogOnlyStaff() || !getConfig.LogOnlyStaff()) {
-
-
-			try {
-				con = DriverManager.getConnection("jdbc:mysql://" + getConfig.MySQLServer() + "/" + getConfig.MySQLDatabase() , getConfig.MySQLUser(), getConfig.MySQLPassword());
-
-				String database = "playerlogger";
-				//Prepared statement
-				pst = con.prepareStatement("INSERT INTO " +database+"(playername, type, time, data, x, y, z, world) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
-				//Values
-				pst.setString(1, playername);
-				pst.setString(2, type);
-				pst.setLong(3, time);
-				pst.setString(4, data);
-				pst.setDouble(5, x);
-				pst.setDouble(6, y);
-				pst.setDouble(7, z);
-				pst.setString(8, worldname);
-				//Do the MySQL query
-				pst.executeUpdate();
-			} catch (SQLException ex) {
-				System.out.print(ex);
+	public void add(final String playername, final String type, final String data, final double x, final double y, final double z, final String worldname, final Boolean staff) {
+		Bukkit.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+		    @Override
+		    public void run() {			
+				PreparedStatement pst = null;
+				long time = System.currentTimeMillis()/1000; //Unix time
+				//Checking if they should be logged
+				if (staff && getConfig.LogOnlyStaff() || !getConfig.LogOnlyStaff()) {
+					try {
+						String database = "playerlogger";
+						//Prepared statement
+						pst = con.prepareStatement("INSERT INTO " +database+"(playername, type, time, data, x, y, z, world) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+						//Values
+						pst.setString(1, playername);
+						pst.setString(2, type);
+						pst.setLong(3, time);
+						pst.setString(4, data);
+						pst.setDouble(5, x);
+						pst.setDouble(6, y);
+						pst.setDouble(7, z);
+						pst.setString(8, worldname);
+						//Do the MySQL query
+						pst.executeUpdate();
+					} catch (SQLException ex) {
+						ex.printStackTrace();
+					}
+				}
 			}
-		}	
+		});
 	}
 }
